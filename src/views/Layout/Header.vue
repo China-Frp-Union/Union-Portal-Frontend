b45c56f5e233b323e4d3c83f4b95668fae1eb09a
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-14 10:23:34 +0800

    更好的头部菜单样式

--- src/views/Layout/Header.vue
+++ src/views/Layout/Header.vue
@@ -1,16 +1,17 @@
 <template>
-    <n-space id="header" justify="space-between">
-        <n-gradient-text :size="24" type="warning">
-            中国内网穿透联盟管理系统
-        </n-gradient-text>
-        <Menu></Menu>
-    </n-space>
+  <n-space id="header" justify="space-between">
+    <n-gradient-text :size="24" type="warning">
+      中国内网穿透联盟管理系统
+    </n-gradient-text>
+  </n-space>
+  <Menu></Menu>
 </template>
 <script setup>
 import Menu from "./Menu.vue";
 </script>
 <style scoped>
 #header {
-    margin: 16px;
+  margin-block: 10px;
+  margin-left: 20px;
 }
 </style>
--- src/views/Layout/Menu.vue
+++ src/views/Layout/Menu.vue
@@ -1,9 +1,19 @@
 <template>
-    <n-modal v-model:show="showResetPassword" :mask-closable="false" preset="card" style="width: 600px"
-    title="修改密码">
+  <n-modal
+    v-model:show="showResetPassword"
+    :mask-closable="false"
+    preset="card"
+    style="width: 600px"
+    title="修改密码"
+  >
     <resetPassword @updateSuccess="handleUpdateSuccess()"></resetPassword>
   </n-modal>
-    <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" responsive />
+  <n-menu
+    v-model:value="activeKey"
+    mode="horizontal"
+    :options="menuOptions"
+    responsive
+  />
 </template>
 
 <script setup>
@@ -14,7 +24,7 @@ import {
   BookOutline as BookIcon,
   HomeOutline as HomeIcon,
   PersonOutline as PersonIcon,
-  WineOutline as WineIcon
+  WineOutline as WineIcon,
 } from "@vicons/ionicons5";
 import store from "@utils/stores/profile.js";
 import router from "@src/router/index.js";
@@ -24,76 +34,81 @@ function renderIcon(icon) {
   return () => h(NIcon, null, { default: () => h(icon) });
 }
 
-function handleUpdateSuccess(){
+function handleUpdateSuccess() {
   showResetPassword.value = false;
 }
 
 const showResetPassword = ref(false);
 const menuOptions = [
   {
-    label: () => h(
-      RouterLink,
-      {
-        to: {
-          name: "MainPage",
-        }
-      },
-      { default: () => "首页" }
-    ),
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "MainPage",
+          },
+        },
+        { default: () => "首页" }
+      ),
     key: "MainPage",
-    icon: renderIcon(HomeIcon)
+    icon: renderIcon(HomeIcon),
   },
   {
-    label: () => h(
-      RouterLink,
-      {
-        to: {
-          name: "blacklist",
-        }
-      },
-      { default: () => "黑名单列表" }
-    ),
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "blacklist",
+          },
+        },
+        { default: () => "黑名单列表" }
+      ),
     key: "blacklist",
-    icon: renderIcon(HomeIcon)
+    icon: renderIcon(HomeIcon),
   },
   {
-    label: () => h(
-      RouterLink,
-      {
-        to: {
-          name: "apply",
-        }
-      },
-      { default: () => "申请管理" }
-    ),
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "apply",
+          },
+        },
+        { default: () => "申请管理" }
+      ),
     key: "apply",
-    icon: renderIcon(WineIcon)
+    icon: renderIcon(WineIcon),
   },
   {
-    label: () => h(
-      RouterLink,
-      {
-        to: {
-          name: "app",
-        }
-      },
-      { default: () => "应用管理" }
-    ),
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "app",
+          },
+        },
+        { default: () => "应用管理" }
+      ),
     key: "app",
-    icon: renderIcon(BookIcon)
+    icon: renderIcon(BookIcon),
   },
   {
-    label: () => h(
-      RouterLink,
-      {
-        to: {
-          name: "site",
-        }
-      },
-      { default: () => "站点管理" }
-    ),
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "site",
+          },
+        },
+        { default: () => "站点管理" }
+      ),
     key: "site",
-    icon: renderIcon(PersonIcon)
+    icon: renderIcon(PersonIcon),
   },
   {
     label: "用户",
@@ -106,67 +121,71 @@ const menuOptions = [
         key: "account",
         children: [
           {
-            label: () => h(
+            label: () =>
+              h(
                 RouterLink,
                 {
                   to: {
                     name: "login",
-                  }
+                  },
                 },
                 { default: () => "登录" }
-            ),
+              ),
             key: "login",
-            icon: renderIcon(PersonIcon)
+            icon: renderIcon(PersonIcon),
           },
           {
-            label: () => h(
+            label: () =>
+              h(
                 RouterLink,
                 {
                   to: {
                     name: "join",
-                  }
+                  },
                 },
                 { default: () => "申请加入" }
-            ),
+              ),
             key: "join",
-            icon: renderIcon(PersonIcon)
-          }
-        ]
-      }
-    ]
+            icon: renderIcon(PersonIcon),
+          },
+        ],
+      },
+    ],
   },
   {
     label: store.getters.get_username,
     key: "username_label",
     children: [
-    {
-        label: () => h(
-          "a",
-          {
-            onClick: () => {
-              showResetPassword.value = true;
-            }
-          },
-          "修改密码"
-        ),
-        key: "reset_password"
+      {
+        label: () =>
+          h(
+            "a",
+            {
+              onClick: () => {
+                showResetPassword.value = true;
+              },
+            },
+            "修改密码"
+          ),
+        key: "reset_password",
       },
       {
-        label: () => h(
+        label: () =>
+          h(
             "a",
             {
               onClick: () => {
                 store.commit("delete_token");
                 store.commit("delete_user_info");
                 router.push("/login");
-              }
+              },
             },
             "退出登录"
-        ),
-        key: "logout"
-      }
-    ]
-  }
+          ),
+        key: "logout",
+      },
+    ],
+  },
 ];
 
 const token = store.getters.get_token;
@@ -175,7 +194,7 @@ const permission = Number(store.getters.get_permission);
 if (!token) {
   // 删除 申请管理 和 应用管理 和 站点管理
   menuOptions.splice(2, 3);
-  menuOptions.splice(3,1)
+  menuOptions.splice(3, 1);
 } else if (permission === 0) {
   // 普通用户 删除 申请管理
   menuOptions.splice(2, 1);
@@ -191,7 +210,7 @@ if (!token) {
 import { ref } from "vue";
 
 const activeKey = ref("");
-export function handleActiveKeyChange(name){
+export function handleActiveKeyChange(name) {
   activeKey.value = name;
 }
 </script>
