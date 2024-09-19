6e64336d958950fc31c8dd561f0d570e01752ce1
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-20 00:16:12 +0800

    Feat: dark theme

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,25 @@ import loadingBar from "./components/LoadingBar.vue";
 import Message from "./components/Message.vue";
 import DialogBox from "./components/Dialog.vue";
 import Portal from "./views/Portal.vue";
+
+import { computed } from 'vue'
+import { darkTheme, useOsTheme } from "naive-ui";
+
+const osThemeRef = useOsTheme();
+const theme = computed(() => (osThemeRef.value === "dark" ? darkTheme : null));
 </script>
 
 <template>
-  <n-loading-bar-provider>
-    <n-message-provider>
-      <n-dialog-provider>
-        <loadingBar />
-        <Message />
-        <DialogBox />
-        <Portal />
-      </n-dialog-provider>
-    </n-message-provider>
-  </n-loading-bar-provider>
+  <n-config-provider :theme="theme">
+    <n-loading-bar-provider>
+      <n-message-provider>
+        <n-dialog-provider>
+          <loadingBar />
+          <Message />
+          <DialogBox />
+          <Portal />
+        </n-dialog-provider>
+      </n-message-provider>
+    </n-loading-bar-provider>
+  </n-config-provider>
 </template>
